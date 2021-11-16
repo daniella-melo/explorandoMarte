a80174b632c1cfcc673e642a422eb247aa974a93
Daniella <fcmdaniella@gmail.com>
2021-11-16 18:48:22 -0300

    Estrutura Inicial

---
+++ Sonda.java
@@ -0,0 +1,17 @@
+public class Sonda{
+
+    private int X; //coordenada X
+    private int Y; //coordenada Y
+    
+
+    private int L; //faz a sonda virar 90 graus para a esquerda, sem mover a sonda.
+    private int R; //faz a sonda virar 90 graus para a direita, sem mover a sonda.
+    private int M; //faz com que a sonda mova-se para a frente um ponto da malha, mantendo a mesma direção.
+
+
+    public Sonda(){
+
+    }
+
+
+}
